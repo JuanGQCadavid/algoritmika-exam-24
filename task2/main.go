0e17029a4dcfab9683fe599e8a05cc90560bfee2
Juan Gonzalo Quıroz Cadavıd <jquirozcadavid@gmail.com>
2025-01-05 15:18:37 +0200

    Working and iterating, finalizing final report for allowing it to run

--- task2/main.go
+++ task2/main.go
@@ -5,8 +5,10 @@ import (
 	"exam/task3/core/utils"
 	"image"
 	"image/color"
+	"image/draw"
 	"log"
-	"math"
+	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -33,6 +35,13 @@ var (
 
 	// Parallel params
 	maxSubprocess = 8
+
+	// Process time
+	runningLimit time.Duration = time.Minute * 1
+	folderName                 = strings.ReplaceAll(time.Now().Format(time.DateOnly)+"-"+time.Now().Format(time.Kitchen), ":", "_")
+
+	// Hyperparameter
+	crossOverProb float64 = 0.65
 )
 
 func init() {
@@ -50,6 +59,8 @@ func init() {
 			colorPalette[i][j] = colorSelected
 		}
 	}
+
+	os.MkdirAll(folderName, 0755)
 }
 
 func createOne(colorPaletteRange, boxSize int, width uint) [][]int {
@@ -64,38 +75,22 @@ func createOne(colorPaletteRange, boxSize int, width uint) [][]int {
 	return child
 }
 
-func generatePopulation(initPopulationSize, colorPaletteRange, boxSize int, width uint) [][][]int {
-	var (
-		popu = make([][][]int, initPopulationSize)
-	)
-	for i := range popu {
-		popu[i] = createOne(colorPaletteRange, boxSize, width)
-	}
-	return popu
+type PopStat struct {
+	Data  [][]int
+	Score float64
 }
 
-func generateBoxLimits(a image.Image, size int) [][]int {
-	result := make([][]int, 0)
-
-	for y := 0; y < a.Bounds().Dy(); y += size {
-		for x := 0; x < a.Bounds().Dx(); x += size {
-			var (
-				xLimit = x + size
-				yLimit = y + size
-			)
-
-			if xLimit > a.Bounds().Dx() {
-				xLimit = a.Bounds().Dx() - 1
-			}
-			if yLimit > a.Bounds().Dy() {
-				yLimit = a.Bounds().Dy() - 1
-			}
-
-			result = append(result, []int{x, y, xLimit, yLimit})
+func generatePopulation(initPopulationSize, colorPaletteRange, boxSize int, width uint) []PopStat {
+	var (
+		popu = make([]PopStat, initPopulationSize)
+	)
+	for i := range popu {
+		newBorn := createOne(colorPaletteRange, boxSize, width)
+		popu[i] = PopStat{
+			Data: newBorn,
 		}
 	}
-
-	return result
+	return popu
 }
 
 func extractVectorBox(img image.Image, startX, startY, endX, endY int) []color.Color {
@@ -171,29 +166,129 @@ func parallelFitnessFunction(targetImage image.Image, boxSize int, offSprint [][
 	return globalSum
 }
 
+func getNParents(n, limit int) []int {
+	result := make([]int, n)
+
+	for i := range result {
+		for {
+			id := rand.Intn(limit)
+			unique := true
+			for j := 0; j < i; j++ {
+				if result[j] == id {
+					unique = false
+				}
+			}
+
+			if unique {
+				result[i] = id
+				break
+			}
+		}
+	}
+	return result
+}
+
+func Off(father int, others []int, populationArray []PopStat, crossOverProb float64) [][]int {
+	rowSize, colSize := len(populationArray[0].Data), len(populationArray[0].Data[0])
+
+	newBorn := make([][]int, rowSize)
+
+	for i := 0; i < rowSize; i++ {
+		newBorn[i] = make([]int, colSize)
+		for j := 0; j < colSize; j++ {
+			var colorFrom int
+			if rand.Float64() < crossOverProb {
+				getFrom := rand.Intn(len(others) - 1)
+				if getFrom >= len(others) {
+					getFrom = len(others) - 1
+				}
+				colorFrom = others[getFrom]
+			} else {
+				colorFrom = father
+			}
+			newBorn[i][j] = populationArray[colorFrom].Data[i][j]
+		}
+	}
+
+	return newBorn
+}
+
+func DrawOverImage(targetImage image.Image, offSpritn PopStat, boxSize int) image.Image {
+	bounds := targetImage.Bounds()
+	ca := image.NewRGBA(bounds)
+	draw.Draw(ca, bounds, targetImage, bounds.Min, draw.Src)
+	mod := targetImage.Bounds().Dx() / boxSize
+	// rowSize, colSize := len(offSpritn.Data), len(offSpritn.Data[0])
+	for x := 0; x < targetImage.Bounds().Dx(); x++ {
+		for y := 0; y < targetImage.Bounds().Dy(); y++ {
+			colorX, colorY := int(x/mod), int(y/mod)
+
+			cols := colorPalette[offSpritn.Data[colorX][colorY]]
+			ca.Set(x, y, cols[0])
+		}
+	}
+
+	return ca
+}
+
 func main() {
 	var (
-		targetImage image.Image = utils.ReadImage(targetImgPath)
+		targetImageOrigin image.Image = utils.ReadImage(targetImgPath)
+		targetImage                   = resize.Resize(width, width, targetImageOrigin, resize.Lanczos3)
+		initPopulation                = generatePopulation(initPopulationSize, len(colorPalette), boxSize, width)
 	)
-	a := resize.Resize(width, width, targetImage, resize.Lanczos3)
-	utils.SaveImageToPNG("test.png", "./", a)
-	initPopulation := generatePopulation(initPopulationSize, len(colorPalette), boxSize, width)
 
-	log.Println(len(initPopulation[0]), len(initPopulation[0][0]))
-	// log.Println(initPopulation[0][0])
+	// utils.SaveImageToPNG("origin.png", "./", targetImage)
+	// dst := DrawOverImage(targetImage, initPopulation[0], boxSize)
+	// utils.SaveImageToPNG("example.png", "./", dst)
 
-	// fitnessFunction(targetImage, boxSize, int(width), initPopulation[0])
-	log.Print("Single: ")
-	tick := time.Now()
-	s := fitnessFunction(targetImage, boxSize, initPopulation[0])
-	println(int(s))
-	log.Println(time.Since(tick))
+	startTime := time.Now()
+	iterationsCounter := 0
 
-	log.Print("Parallel: ")
-	tick = time.Now()
-	p := parallelFitnessFunction(targetImage, boxSize, initPopulation[0])
-	println(int(p))
-	log.Println(time.Since(tick))
+	for time.Since(startTime) < runningLimit {
+		var (
+			parentsIndex  = getNParents(4, initPopulationSize)
+			offSprint     = Off(parentsIndex[0], parentsIndex[1:], initPopulation, crossOverProb)
+			offFitness    = 0.0
+			fatherFitness = initPopulation[parentsIndex[0]].Score
+			wg            = sync.WaitGroup{}
+		)
 
-	log.Println(math.Abs(s - p))
+		if fatherFitness == 0 {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				fatherFitness = parallelFitnessFunction(targetImage, boxSize, initPopulation[parentsIndex[0]].Data)
+			}()
+		}
+
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			offFitness = parallelFitnessFunction(targetImage, boxSize, offSprint)
+		}()
+		wg.Wait()
+		if offFitness < fatherFitness {
+			log.Println("Off is better than father ", offFitness, " < ", fatherFitness)
+			initPopulation[parentsIndex[0]] = PopStat{
+				Data:  offSprint,
+				Score: offFitness,
+			}
+		}
+		if initPopulation[parentsIndex[0]].Score == 0 {
+			initPopulation[parentsIndex[0]].Score = fatherFitness
+		}
+		iterationsCounter += 1
+	}
+
+	log.Println("Total iterations: ", iterationsCounter)
+
+	// sort.Slice(initPopulation, func(i, j int) bool {
+
+	// })
+
+	// utils.SaveImageToPNG("test.png", "./", a)
+	// p := parallelFitnessFunction(targetImage, boxSize, initPopulation[0])
+	// println(int(p))
+
 }
